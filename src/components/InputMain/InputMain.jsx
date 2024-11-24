51648cc16d851de99ae417c8c22681c51b841953
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-11-24 23:07:52 +0400

    работаю над input

--- src/components/InputMain/InputMain.jsx
+++ src/components/InputMain/InputMain.jsx
@@ -5,11 +5,88 @@ import {
   Box,
   Button,
   CardMedia,
+  TextField,
 } from "@mui/material";
+import { useState } from "react";
+
 const IMAGES = {
   image1: new URL("./img/headerLogo.png", import.meta.url).href,
   image2: new URL("./img/hd-logo1.png", import.meta.url).href,
 };
+
+export function Form() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+  });
+
+  const validateForm = () => {
+    const newErrors = {
+      name: formData.name === "" ? "Name is required" : "",
+      email: formData.email === "" ? "Email is required" : "",
+    };
+    setErrors(newErrors);
+
+    return !Object.values(newErrors).some(Boolean);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: "" });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // Submit the form or perform further actions here
+      console.log("Form data:", formData);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        name="name"
+        label="Name"
+        variant="outlined"
+        color="secondary"
+        sx={{ my: 1 }}
+        fullWidth
+        value={formData.name}
+        onChange={handleInputChange}
+        error={Boolean(errors.name)}
+        helperText={errors.name}
+      />
+      <TextField
+        name="email"
+        label="Email"
+        variant="outlined"
+        fullWidth
+        color="secondary"
+        sx={{ my: 1 }}
+        value={formData.email}
+        onChange={handleInputChange}
+        error={Boolean(errors.email)}
+        helperText={errors.email}
+      />
+      <Button
+        type="submit"
+        color="secondary"
+        sx={{ my: 1 }}
+        variant="contained"
+        fullWidth
+      >
+        Register
+      </Button>
+    </form>
+  );
+}
 
 function InputMain() {
   return (
@@ -40,6 +117,25 @@ function InputMain() {
             image={IMAGES.image1}
             alt="Paella  dish"
           />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "32px",
+            width: "352px",
+          }}
+          className="text__header-input"
+        >
+          <Typography sx={{ fontSize: "14px", fontWeight: 400 }} variant="p">
+            Sign up
+          </Typography>
+          <Typography sx={{ fontSize: "14px", fontWeight: 400 }} variant="p">
+            Sign in
+          </Typography>
+        </Box>
+        <Box className="form__input">
+          <Form />
         </Box>
       </Box>
     </Container>
